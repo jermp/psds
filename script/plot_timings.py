dcb4a059e999b46f183e6691fced2635c1d3aa79
Giulio Ermanno Pibiri <jeis90@gmail.com>
2019-10-29 14:19:40 +0100

    results updated

--- script/plot_timings.py
+++ script/plot_timings.py
@@ -9,7 +9,7 @@ plt.rcParams['axes.facecolor'] = '#f7f7f7'
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.ticker import FuncFormatter
 
-fig, ax = plt.subplots(figsize = (10,5))
+fig, ax = plt.subplots(figsize = (15,10))
 plt.margins(0.01, 0.01)
 
 ax.set_ylabel('ns/query', fontsize = 10)
@@ -43,20 +43,20 @@ for i in range(offset, len(y[types[0]]) + offset):
     x.append(n)
     n *= 2
 
-m = 7
-colors = ['#d53e4f','#fc8d59','#fee08b','#e6f598','#99d594','#3288bd'] #['#ccece6','#99d8c9','#66c2a4','#2ca25f','#006d2c'] #['#bae4bc','#7bccc4','#43a2ca','#0868ac']
+m = 5
+colors = ['#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#e6f598','#abdda4','#66c2a5','#3288bd','#5e4fa2'] #['#d53e4f','#fc8d59','#fee08b','#e6f598','#99d594','#3288bd'] #['#ccece6','#99d8c9','#66c2a4','#2ca25f','#006d2c'] #['#bae4bc','#7bccc4','#43a2ca','#0868ac']
 gray = '#696969'
 markers = ['o', 'X', 's', 'd', 'v', '^', 'P', '*', 'D', 'p']
 
 lines = []
 for i in range(0,len(y)):
-    lines.append(ax.plot(x, y[types[i]], zorder = 1, color = colors[i], alpha = 1, linewidth = 1.7, marker = markers[i], markersize = m))
+    lines.append(ax.plot(x, y[types[i]], zorder = 1, color = colors[i], alpha = 1, linewidth = 1.5, marker = markers[i], markersize = m))
 
 ax.legend(
     ([types[i] for i in range(0,len(types))]),
       fontsize = 9,
-      bbox_to_anchor=(0.005, 0.77),
-      loc=3, ncol=1,
+      bbox_to_anchor=(0.005, 0.87),
+      loc=3, ncol=2,
       borderaxespad = 0.)
 
 pp = PdfPages(args.output_filename + '.pdf')
