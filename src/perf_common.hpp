cb5804201a61594d2c918ce09efd493366a61313
Giulio Ermanno Pibiri <jeis90@gmail.com>
2020-03-29 15:59:01 +0200

    src

--- src/perf_common.hpp
+++ src/perf_common.hpp
@@ -11,17 +11,14 @@ namespace psds {
 namespace benchmarking {
 
 static constexpr uint32_t num_queries = 1000000;
-static constexpr uint32_t max_log = 31;
 static constexpr unsigned value_seed = 13;
 static constexpr unsigned query_seed = 71;
-static constexpr unsigned posit_seed = 89;
 
-static constexpr int8_t deltas[] = {+1, -1};
 static constexpr uint32_t logs[] = {8,  9,  10, 11, 12, 13, 14, 15,
                                     16, 17, 18, 19, 20, 21, 22, 23,
                                     24, 25, 26, 27, 28, 29, 30, 31};
 
-static constexpr uint32_t degree256_heights[] = {
+static constexpr uint32_t fanout256_heights[] = {
     1,  // size = 2^8
     2,  // size = 2^i, with i in [9,16]
     2, 2, 2, 2, 2, 2, 2,
@@ -30,7 +27,7 @@ static constexpr uint32_t degree256_heights[] = {
     4,  // size = 2^i, with i in [25,31]
     4, 4, 4, 4, 4, 4};
 
-static constexpr uint32_t degree64_heights[] = {
+static constexpr uint32_t fanout64_heights[] = {
     2,  // size = 2^i, with i in [8,12]
     2, 2, 2, 2,
     3,  // size = 2^i, with i in [13,18]
@@ -38,75 +35,52 @@ static constexpr uint32_t degree64_heights[] = {
     4,  // size = 2^i, with i in [19,24]
     4, 4, 4, 4, 4};
 
-#define GEN_TYPE_NODE256(i, Node) \
-    tree_epi32<benchmarking::degree256_heights[i], Node>
-#define GEN_TYPE_NODE64(i, Node) \
-    tree_epi32<benchmarking::degree64_heights[i], Node>
+static constexpr uint32_t fanout16_heights[] = {
+    2,           // size = 2^8
+    3, 3, 3, 3,  // size = 2^i, with i in [9,12]
+    4, 4, 4, 4   // size = 2^i, with i in [13,16]
+};
 
-#define MAIN                                                            \
-    if (argc < 2) {                                                     \
-        std::cout << argv[0] << " type" << std::endl;                   \
-        return 1;                                                       \
-    }                                                                   \
-    std::string type = argv[1];                                         \
-    if (type == "tree256u") {                                           \
-        perf_test_tree_epi32_node256<node256u>();                       \
-    } else if (type == "tree256uu") {                                   \
-        perf_test_tree_epi32_node256<node256u_unbuffered>();            \
-    } else if (type == "tree256s") {                                    \
-        perf_test_tree_epi32_node256<node256s>();                       \
-    } else if (type == "tree64u") {                                     \
-        perf_test_tree_epi32_node64<node64u>();                         \
-    } else if (type == "tree64s") {                                     \
-        perf_test_tree_epi32_node64<node64s>();                         \
-    } else if (type == "st") {                                          \
-        perf_test<segment_tree_type>();                                 \
-    } else if (type == "ft") {                                          \
-        perf_test<fenwick_tree_type>();                                 \
-    } else if (type == "tft256u") {                                     \
-        perf_test<truncated_fenwick_tree_epi32<node256u>>();            \
-    } else if (type == "tft256uu") {                                    \
-        perf_test<truncated_fenwick_tree_epi32<node256u_unbuffered>>(); \
-    } else if (type == "tft256s") {                                     \
-        perf_test<truncated_fenwick_tree_epi32<node256s>>();            \
-    } else if (type == "tft64u") {                                      \
-        perf_test<truncated_fenwick_tree_epi32<node64u>>();             \
-    } else if (type == "tft64s") {                                      \
-        perf_test<truncated_fenwick_tree_epi32<node64s>>();             \
-    } else if (type == "bft256u") {                                     \
-        perf_test<blocked_fenwick_tree_epi32<node256u>>();              \
-    } else if (type == "bft256s") {                                     \
-        perf_test<blocked_fenwick_tree_epi32<node256s>>();              \
-    } else if (type == "bft64u") {                                      \
-        perf_test<blocked_fenwick_tree_epi32<node64u>>();               \
-    } else if (type == "bft64s") {                                      \
-        perf_test<blocked_fenwick_tree_epi32<node64s>>();               \
-    } else {                                                            \
-        return 1;                                                       \
+#define GEN_TYPE_NODE256(i, Node) \
+    segment_tree_simd<benchmarking::fanout256_heights[i], Node>
+#define GEN_TYPE_NODE64(i, Node) \
+    segment_tree_simd<benchmarking::fanout64_heights[i], Node>
+#define GEN_TYPE_NODE16(i, Node) \
+    segment_tree_simd<benchmarking::fanout16_heights[i], Node>
+
+#define MAIN                                                         \
+    if (argc < 2) {                                                  \
+        std::cout << argv[0] << " type" << std::endl;                \
+        return 1;                                                    \
+    }                                                                \
+    std::string type = argv[1];                                      \
+    if (type == "st") {                                              \
+        perf_test<segment_tree>();                                   \
+    } else if (type == "ft") {                                       \
+        perf_test<fenwick_tree>();                                   \
+    } else {                                                         \
+        std::cout << "unknown type \"" << type << "\"" << std::endl; \
+        return 1;                                                    \
     }
 
-#define PERF_SUM                                                   \
-    std::cout << "performing " << queries.size() << " SUM queries" \
-              << std::endl;                                        \
-    int64_t total = 0;                                             \
-    essentials::timer_type t;                                      \
-    static const int runs = 10 + 1;                                \
-    for (int run = 0; run != runs; ++run) {                        \
-        t.start();                                                 \
-        for (auto q : queries) total += tree.sum(q);               \
-        t.stop();                                                  \
-    }                                                              \
-    std::cout << tree.name() << " -- ";                            \
-    std::cout << total << std::endl;                               \
-    double min = t.min();                                          \
-    double avg_ns_query = (min * 1000) / queries.size();           \
-    std::cout << "Mean per run: " << min << " [musec]\n";          \
-    std::cout << "Mean per query: " << avg_ns_query << " [ns]";    \
+#define PERF_SUM                                                \
+    std::cout << tree.name() << "\n";                           \
+    int64_t total = 0;                                          \
+    essentials::timer_type t;                                   \
+    static const int runs = 10 + 1;                             \
+    for (int run = 0; run != runs; ++run) {                     \
+        t.start();                                              \
+        for (auto q : queries) total += tree.sum(q);            \
+        t.stop();                                               \
+    }                                                           \
+    std::cout << "# ignore: " << total << std::endl;            \
+    double min = t.min();                                       \
+    double avg_ns_query = (min * 1000) / queries.size();        \
+    std::cout << "Mean per query: " << avg_ns_query << " [ns]"; \
     std::cout << std::endl;
 
 #define PERF_UPDATE                                                   \
-    std::cout << "performing " << queries.size() << " UPDATE queries" \
-              << std::endl;                                           \
+    std::cout << tree.name() << "\n";                                 \
     essentials::timer_type t;                                         \
     static const int runs = 5 + 1;                                    \
     for (int run = 0; run != runs; ++run) {                           \
@@ -114,11 +88,9 @@ static constexpr uint32_t degree64_heights[] = {
         for (auto const& q : queries) tree.update(q.first, q.second); \
         t.stop();                                                     \
     }                                                                 \
-    std::cout << tree.name() << " -- ";                               \
-    std::cout << tree.sum(n - 1) << std::endl;                        \
+    std::cout << "# ignore: " << tree.sum(n - 1) << std::endl;        \
     double min = t.min();                                             \
     double avg_ns_query = (min * 1000) / queries.size();              \
-    std::cout << "Mean per run: " << min << " [musec]\n";             \
     std::cout << "Mean per query: " << avg_ns_query << " [ns]";       \
     std::cout << std::endl;
 
--- src/perf_sum.cpp
+++ src/perf_sum.cpp
@@ -5,105 +5,99 @@ using namespace psds;
 #define EXE_PERF_SUM(TREE, i)                                  \
     {                                                          \
         TREE tree;                                             \
-        uint32_t n = uint32_t(1) << benchmarking::logs[i];     \
+        uint64_t n = uint64_t(1) << benchmarking::logs[i];     \
         std::cout << "### n = " << n << std::endl;             \
         {                                                      \
-            std::vector<int32_t> input(n);                     \
+            std::vector<int64_t> input(n);                     \
             std::generate(input.begin(), input.end(),          \
                           [&] { return distr_values.gen(); }); \
             tree.build(input.data(), n);                       \
         }                                                      \
-        essentials::uniform_int_rng<int32_t> distr_queries(    \
+        essentials::uniform_int_rng<uint32_t> distr_queries(   \
             0, n - 1, benchmarking::query_seed);               \
         std::generate(queries.begin(), queries.end(),          \
                       [&] { return distr_queries.gen(); });    \
         PERF_SUM                                               \
-        std::cerr << avg_ns_query;                             \
-        if (benchmarking::logs[i] != benchmarking::max_log) {  \
-            std::cerr << ", ";                                 \
-            std::cout << std::endl;                            \
-        }                                                      \
+        json += std::to_string(avg_ns_query) + ',';            \
     }
 
-template <typename Node>
-void perf_test_tree_epi32_node256() {
-    essentials::uniform_int_rng<int32_t> distr_values(-100, 100,
-                                                      benchmarking::value_seed);
-    std::vector<uint32_t> queries(benchmarking::num_queries);
+// template <typename Node>
+// void perf_test_tree_epi32_node256() {
+//     essentials::uniform_int_rng<int32_t> distr_values(-100, 100,
+//                                                       benchmarking::value_seed);
+//     std::vector<uint32_t> queries(benchmarking::num_queries);
 
-    std::cerr << "{";
-    std::cerr << "\"type\": \"" << GEN_TYPE_NODE256(0, Node)::name() << "\", ";
-    std::cerr << "\"timings\": [";
+//     std::cerr << "{";
+//     std::cerr << "\"type\": \"" << GEN_TYPE_NODE256(0, Node)::name() << "\",
+//     "; std::cerr << "\"timings\": [";
 
-    EXE_PERF_SUM(GEN_TYPE_NODE256(0, Node), 0)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(1, Node), 1)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(2, Node), 2)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(3, Node), 3)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(4, Node), 4)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(5, Node), 5)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(6, Node), 6)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(7, Node), 7)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(8, Node), 8)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(9, Node), 9)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(10, Node), 10)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(11, Node), 11)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(12, Node), 12)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(13, Node), 13)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(14, Node), 14)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(15, Node), 15)
-    EXE_PERF_SUM(GEN_TYPE_NODE256(16, Node), 16)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(0, Node), 0)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(1, Node), 1)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(2, Node), 2)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(3, Node), 3)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(4, Node), 4)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(5, Node), 5)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(6, Node), 6)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(7, Node), 7)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(8, Node), 8)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(9, Node), 9)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(10, Node), 10)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(11, Node), 11)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(12, Node), 12)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(13, Node), 13)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(14, Node), 14)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(15, Node), 15)
+//     EXE_PERF_SUM(GEN_TYPE_NODE256(16, Node), 16)
 
-    // EXE_PERF_SUM(GEN_TYPE_NODE256(17, Node), 17)
-    // EXE_PERF_SUM(GEN_TYPE_NODE256(18, Node), 18)
-    // EXE_PERF_SUM(GEN_TYPE_NODE256(19, Node), 19)
-    // EXE_PERF_SUM(GEN_TYPE_NODE256(20, Node), 20)
-    // EXE_PERF_SUM(GEN_TYPE_NODE256(21, Node), 21)
-    // EXE_PERF_SUM(GEN_TYPE_NODE256(22, Node), 22)
-    // EXE_PERF_SUM(GEN_TYPE_NODE256(23, Node), 23)
+//     // EXE_PERF_SUM(GEN_TYPE_NODE256(17, Node), 17)
+//     // EXE_PERF_SUM(GEN_TYPE_NODE256(18, Node), 18)
+//     // EXE_PERF_SUM(GEN_TYPE_NODE256(19, Node), 19)
+//     // EXE_PERF_SUM(GEN_TYPE_NODE256(20, Node), 20)
+//     // EXE_PERF_SUM(GEN_TYPE_NODE256(21, Node), 21)
+//     // EXE_PERF_SUM(GEN_TYPE_NODE256(22, Node), 22)
+//     // EXE_PERF_SUM(GEN_TYPE_NODE256(23, Node), 23)
 
-    std::cerr << "]}" << std::endl;
-}
+//     std::cerr << "]}" << std::endl;
+// }
 
-template <typename Node>
-void perf_test_tree_epi32_node64() {
-    essentials::uniform_int_rng<int32_t> distr_values(-100, 100,
-                                                      benchmarking::value_seed);
-    std::vector<uint32_t> queries(benchmarking::num_queries);
+// template <typename Node>
+// void perf_test_tree_epi32_node64() {
+//     essentials::uniform_int_rng<int32_t> distr_values(-100, 100,
+//                                                       benchmarking::value_seed);
+//     std::vector<uint32_t> queries(benchmarking::num_queries);
 
-    std::cerr << "{";
-    std::cerr << "\"type\": \"" << GEN_TYPE_NODE64(0, Node)::name() << "\", ";
-    std::cerr << "\"timings\": [";
+//     std::cerr << "{";
+//     std::cerr << "\"type\": \"" << GEN_TYPE_NODE64(0, Node)::name() << "\",
+//     "; std::cerr << "\"timings\": [";
 
-    EXE_PERF_SUM(GEN_TYPE_NODE64(0, Node), 0)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(1, Node), 1)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(2, Node), 2)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(3, Node), 3)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(4, Node), 4)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(5, Node), 5)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(6, Node), 6)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(7, Node), 7)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(8, Node), 8)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(9, Node), 9)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(10, Node), 10)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(11, Node), 11)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(12, Node), 12)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(13, Node), 13)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(14, Node), 14)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(15, Node), 15)
-    EXE_PERF_SUM(GEN_TYPE_NODE64(16, Node), 16)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(0, Node), 0)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(1, Node), 1)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(2, Node), 2)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(3, Node), 3)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(4, Node), 4)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(5, Node), 5)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(6, Node), 6)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(7, Node), 7)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(8, Node), 8)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(9, Node), 9)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(10, Node), 10)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(11, Node), 11)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(12, Node), 12)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(13, Node), 13)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(14, Node), 14)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(15, Node), 15)
+//     EXE_PERF_SUM(GEN_TYPE_NODE64(16, Node), 16)
 
-    std::cerr << "]}" << std::endl;
-}
+//     std::cerr << "]}" << std::endl;
+// }
 
 template <typename Tree>
 void perf_test() {
-    essentials::uniform_int_rng<int32_t> distr_values(-100, 100,
+    essentials::uniform_int_rng<int64_t> distr_values(-100, 100,
                                                       benchmarking::value_seed);
     std::vector<uint32_t> queries(benchmarking::num_queries);
 
-    std::cerr << "{";
-    std::cerr << "\"type\": \"" << Tree::name() << "\", ";
-    std::cerr << "\"timings\": [";
+    std::string json("{type:\"" + Tree::name() + "\", timings:[");
 
     EXE_PERF_SUM(Tree, 0)
     EXE_PERF_SUM(Tree, 1)
@@ -118,11 +112,10 @@ void perf_test() {
     EXE_PERF_SUM(Tree, 10)
     EXE_PERF_SUM(Tree, 11)
     EXE_PERF_SUM(Tree, 12)
-    EXE_PERF_SUM(Tree, 13)
-    EXE_PERF_SUM(Tree, 14)
-    EXE_PERF_SUM(Tree, 15)
-    EXE_PERF_SUM(Tree, 16)
-
+    // EXE_PERF_SUM(Tree, 13)
+    // EXE_PERF_SUM(Tree, 14)
+    // EXE_PERF_SUM(Tree, 15)
+    // EXE_PERF_SUM(Tree, 16)
     // EXE_PERF_SUM(Tree, 17)
     // EXE_PERF_SUM(Tree, 18)
     // EXE_PERF_SUM(Tree, 19)
@@ -131,7 +124,9 @@ void perf_test() {
     // EXE_PERF_SUM(Tree, 22)
     // EXE_PERF_SUM(Tree, 23)
 
-    std::cerr << "]}" << std::endl;
+    json.pop_back();
+    json += "]}";
+    std::cerr << json << std::endl;
 }
 
 int main(int argc, char** argv) {
