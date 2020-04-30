8ff1581f2e02845a56304b0a5f10d5c9110e6f58
Giulio Ermanno Pibiri <jeis90@gmail.com>
2020-04-30 18:35:59 +0200

    modified segment-tree bottom-up

--- include/trees/segment_tree_bottomup.hpp
+++ include/trees/segment_tree_bottomup.hpp
@@ -11,11 +11,9 @@ struct segment_tree_bottomup {
         m_size = n;
         m_tree.resize(m);
         m_begin = (1ULL << uint64_t(ceil(log2(n)))) - 1;
-
         uint64_t i = 0;
         for (; m_begin + i != m; ++i) m_tree[m_begin + i] = input[i];
         for (uint64_t j = 0; i != n; ++i, ++j) m_tree[n - 1 + j] = input[i];
-
         build(0);
     }
 
@@ -28,14 +26,8 @@ struct segment_tree_bottomup {
         p -= (p >= m_tree.size()) * m_size;
         int64_t sum = m_tree[p];
         while (p) {
-            uint64_t pp = (p - 1) / 2;  // parent
-            if (((p + 1) & 3) == 0) {
-                pp = (pp - 1) / 2;  // grand parent
-            } else if ((p & 1) == 0) {
-                sum += m_tree[pp];  // p is right child
-            }
-            // sum += ((p & 1) == 0) * m_tree[pp];
-            p = pp;
+            if ((p & 1) == 0) sum += m_tree[p - 1];
+            p = (p - 1) / 2;
         }
         return sum;
     }
@@ -43,12 +35,9 @@ struct segment_tree_bottomup {
     void update(uint64_t i, int64_t delta) {
         uint64_t p = m_begin + i;
         p -= (p >= m_tree.size()) * m_size;
-        m_tree[p] += delta;
         while (p) {
-            uint64_t pp = (p - 1) / 2;
-            if ((p & 1) == 1) m_tree[pp] += delta;  // p is left child
-            // m_tree[pp] += ((p & 1) == 1) * delta;
-            p = pp;
+            m_tree[p] += delta;
+            p = (p - 1) / 2;
         }
     }
 
@@ -57,12 +46,71 @@ private:
     std::vector<int64_t> m_tree;
 
     int64_t build(uint64_t p) {
-        uint64_t l = 2 * p + 1;
-        if (l >= m_tree.size()) return m_tree[p];  // leaf
-        int64_t l_sum = build(l), r_sum = build(l + 1);
-        m_tree[p] = l_sum;
-        return l_sum + r_sum;
+        if (p >= m_tree.size()) return 0;
+        if (p >= m_size - 1) return m_tree[p];  // leaf
+        int64_t l_sum = build(2 * p + 1);
+        int64_t r_sum = build(2 * p + 2);
+        return m_tree[p] = l_sum + r_sum;
     }
 };
+
+/* in this version, each internal node stores the sum of (the leaves descending
+   from) its left sub-tree  */
+// struct segment_tree_bottomup {
+//     template <typename T>
+//     void build(T const* input, uint64_t n) {
+//         uint64_t m = 2 * n - 1;
+//         m_size = n;
+//         m_tree.resize(m);
+//         m_begin = (1ULL << uint64_t(ceil(log2(n)))) - 1;
+
+//         uint64_t i = 0;
+//         for (; m_begin + i != m; ++i) m_tree[m_begin + i] = input[i];
+//         for (uint64_t j = 0; i != n; ++i, ++j) m_tree[n - 1 + j] = input[i];
+
+//         build(0);
+//     }
+
+//     static std::string name() {
+//         return "segment_tree_bottomup";
+//     }
+
+//     int64_t sum(uint64_t i) const {
+//         uint64_t p = m_begin + i;
+//         p -= (p >= m_tree.size()) * m_size;
+//         int64_t sum = m_tree[p];
+//         while (p) {
+//             uint64_t parent = (p - 1) / 2;
+//             if ((p & 1) == 0) sum += m_tree[parent];  // p is right child
+//             // sum += ((p & 1) == 0) * m_tree[parent];
+//             p = parent;
+//         }
+//         return sum;
+//     }
+
+//     void update(uint64_t i, int64_t delta) {
+//         uint64_t p = m_begin + i;
+//         p -= (p >= m_tree.size()) * m_size;
+//         m_tree[p] += delta;
+//         while (p) {
+//             uint64_t parent = (p - 1) / 2;
+//             if ((p & 1) == 1) m_tree[parent] += delta;  // p is left child
+//             // m_tree[parent] += ((p & 1) == 1) * delta;
+//             p = parent;
+//         }
+//     }
+
+// private:
+//     uint64_t m_size, m_begin;
+//     std::vector<int64_t> m_tree;
+
+//     int64_t build(uint64_t p) {
+//         uint64_t l = 2 * p + 1;
+//         if (l >= m_tree.size()) return m_tree[p];  // leaf
+//         int64_t l_sum = build(l), r_sum = build(l + 1);
+//         m_tree[p] = l_sum;
+//         return l_sum + r_sum;
+//     }
+// };
 
 }  // namespace psds
